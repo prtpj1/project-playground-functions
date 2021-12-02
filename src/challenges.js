80a8f820f340b0e4b0aa8d52bb8e9f68ede92ef4
Paulo Porto <prtpj1@gmail.com>
2021-12-02 20:19:48 -0300

    Req 8 - Done

--- src/challenges.js
+++ src/challenges.js
@@ -35,19 +35,42 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(number) {
   let hNumber;
+  return number;
 
-  for (let i = 0; i <= number.lenght - 1; i = +1) {}
+  // for (let i = 0; i <= number.lenght - 1; i = +1) {}
 }
-console.log(highestCount(9, 1, 2, 3, 9, 5, 7));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dCat1 = mouse - cat1;
+  let dCat2 = mouse - cat2;
+
+  if (dCat2 > dCat1) {
+    return 'cat2';
+  } else if (dCat1 > dCat2) {
+    return 'cat1';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
+// console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let fiz;
+  let buz;
+  for (let i = 0; i <= array.length - 1; i = i + 1) {
+    if ((array[i] % 3 == 0) && (array[i] % 5 == 0)){
+      array[i] = 'fizzBuzz';
+    } else if (array[i] % 5 == 0) {
+      array[i] = 'buzz';
+    } else if (array[i] % 3 == 0) {
+      array[i] = 'fizz';
+    } else {
+      array[i] = 'bug!';
+    }
+  }
+  return array;
 }
 
 // Desafio 9
