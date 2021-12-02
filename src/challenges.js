3d581f9399b3bcae9d7753a9e5db7e2c14ad2955
Paulo Porto <prtpj1@gmail.com>
2021-12-02 16:49:12 -0300

    Req 3 - Done

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+
+function splitSentence(str) {
+  let sentence = str.split(' ');
+  return sentence;
 }
 
 // Desafio 4
